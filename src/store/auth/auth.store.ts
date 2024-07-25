dcd5d0ab681ccc6bf45a82de5df2880d949f461b
leonardo de la cruz <leonardodelacruz03@hotmail.com>
2024-07-24 21:39:04 -0500

    added auth functionality with store and services

--- src/store/auth/auth.store.ts
+++ src/store/auth/auth.store.ts
@@ -3,15 +3,23 @@ import { persist } from "zustand/middleware"
 
 type AuthState = {
   token: string
+  isAuthenticated: boolean
 }
 
 type AuthActions = {
   setToken: (token: string) => void
+  logout: () => void
 }
 
 export const useAuthStore = create(persist<AuthState & AuthActions>((set) => ({
   token: "",
+  isAuthenticated: false,
   setToken: (token: string) => set(() => ({
-    token
+    token,
+    isAuthenticated: !!token
+  })),
+  logout: () => set(() => ({
+    token: "",
+    isAuthenticated: false
   }))
 }), {name: 'authStorage'}))
